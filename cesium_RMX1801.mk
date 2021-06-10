834a6972527316af7af1e1bc025cbb7133692bc7
Aditya Singh <adityasingh1768@gmail.com>
2021-06-10 13:52:36 +0530

    RMX1801: switch to redfin june fp/desc

--- cesium_RMX1801.mk
+++ cesium_RMX1801.mk
@@ -41,6 +41,6 @@ TARGET_VENDOR_DEVICE_NAME := RMX1801
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1801" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys"
