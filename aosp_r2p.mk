3749e9a8caf4a27e3fc5235792f147e9b92bc4d4
GiaSen <giammasena@gmail.com>
2021-08-31 15:02:27 +0000

    r2p: Disable Live wallpapers

--- aosp_r2p.mk
+++ aosp_r2p.mk
@@ -16,7 +16,9 @@ $(call inherit-product, device/realme/r2p/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_r2p
 PRODUCT_DEVICE := r2p
 PRODUCT_MANUFACTURER := OPPO
