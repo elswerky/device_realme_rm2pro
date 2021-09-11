cc2b0894dfea2064c06541e9ebf1f8d3590faab5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-11 07:28:27 +0000

    r2p: Enable AOSP WFD
    Co-authored-by: Marc Bourgoin <themard85@gmail.com>
    
    Change-Id: I49d46c4eb27ccef6e2ae678e8eccc10c2e5257aa

--- device.mk
+++ device.mk
@@ -427,11 +427,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libaacwrapper \
-    libnl
-
 # Qcom
 PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_BOARD_PLATFORM := sdm660
@@ -439,7 +434,3 @@ PRODUCT_BOARD_PLATFORM := sdm660
 # Signapk
 PRODUCT_HOST_PACKAGES += \
     signapk
-
-# WFD
-PRODUCT_BOOT_JARS += \
-    WfdCommon
