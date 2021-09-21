65b654fbbc6b63a3d6e12c949a2947029e6456ea
Shazu-xD <verifiedgamer99@gmail.com>
2021-09-21 14:10:31 +0000

    r2p: Xtended bringup!

--- xtended_r2p.mk
+++ xtended_r2p.mk
@@ -13,13 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 $(call inherit-product, device/realme/r2p/device.mk)
 
 # Inherit some PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_r2p
+PRODUCT_NAME := xtended_r2p
 PRODUCT_DEVICE := r2p
 PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BRAND := OPPO
